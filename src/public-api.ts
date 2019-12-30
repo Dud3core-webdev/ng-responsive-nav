390984319b26466da1d63b0e19e183c6b62fa0a8
Dudecor3 <freeman.marc880@gmail.com>
2019-12-30 15:53:38 +0000

    exposed types in public api

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,3 +1,9 @@
-export * from './app/ng-responsive-nav.component';
-export * from './app/ng-responsive-nav.module';
 export * from './app/window/window.provider';
+export * from './app/config/nav.config';
+export * from './app/config/nav-routes.config';
+export * from './app/config/nav-brand.config';
+
+export { NgResponsiveNavModule } from './app/ng-responsive-nav.module';
+export { NgResponsiveNavComponent } from './app/ng-responsive-nav.component';
+export { DesktopNavigationComponent } from './app/desktop-navigation/desktop-navigation.component';
+export { MobileNavigationComponent } from './app/mobile-navigation/mobile-navigation.component';
